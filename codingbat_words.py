5bc23d0c6ca47967dac834c6ce7fe6de424fa45e
Eduardo RP <eduar.do1@outlook.com>
2024-11-07 10:39:54 -0600

    Prueba

---
+++ codingbat_words.py
@@ -0,0 +1,14 @@
+def word_multiple(strings):
+    word_map = {}
+    for w in strings:
+        if w not in word_map:
+            word_map[w] = False
+        else:
+            word_map[w] = True
+    return word_map
+
+# Datos Prueba
+strings = ["platano", "platano", "manzana", "manzana", "uva", "Sandia"]
+Resultado = word_multiple(strings)
+
+print(Resultado)
